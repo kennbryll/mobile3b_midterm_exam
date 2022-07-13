b22fff9c32ebcdd00f74af41409beebbaa8af2bd
Kenn Bryll <kennbryllcatubay@gmail.com>
2022-07-13 08:32:19 +0800

    added models, fixed bugs on screens

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:fakestore_midterm/services/api_service.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import 'screens/login_screen.dart';
+
+void setupLocator() {
+  GetIt.I.registerLazySingleton(() => ApiService());
+}
+
+void main() {
+  setupLocator();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fake Store',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
